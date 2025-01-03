9d1f7e32be4c91e89c372029217b94bed42fb1bf
Yuta Sakusabe <s23c1062mq@s.chibakoudai.jp>
2025-01-03 17:12:31 +0900

    talk

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,25 +1,21 @@
-#!/usr/bin/python3
-# SPDX-FileCopyrightText: 2024 Yuta Sakusabe <s23c1062mq@s.chibakoudai.jp>
-# SPDX-License-Identifier: BSD-3-Clause
-
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32
-import psutil
+from std_msgs.msg import Int16
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Float32, "cpu_usage", 10)
+pub = node.create_publisher(Int16, "countup", 10)
+n = 0
+
 
 def cb():
-    # 現在のCPU使用率を取得
-    cpu_usage = psutil.cpu_percent(interval=None)  # interval=Noneで直近の平均を取得
-    msg = Float32()
-    msg.data = cpu_usage
+    global n
+    msg = Int16()
+    msg.data = n
     pub.publish(msg)
-    print(f"CPU Usage: {msg.data}%")
+    n += 1
+
 
 def main():
-    # 1秒ごとにCPU使用率を取得して送信
-    node.create_timer(1.0, cb)
+    node.create_timer(0.5, cb)
     rclpy.spin(node)

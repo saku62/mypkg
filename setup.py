05d5991b7d47bb4d06667bc5e8a8658a266b08f9
Yuta Sakusabe <s23c1062mq@s.chibakoudai.jp>
2025-01-03 16:47:09 +0900

    set

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import  setup
 import os
 from glob import glob
 
@@ -7,13 +7,14 @@ package_name = 'mypkg'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py'))
-    ],
+            (os.path.join('share', package_name), glob('launch/*.launch.py'))
+        ],
+
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Yuta Sakusabe',
